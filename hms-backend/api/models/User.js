631ec855297ba5724e0f8a872028702c0b4f9928
mohhaauu <mohhaauu12@gmail.com>
2024-09-03 19:08:35 +0200

    Updated the user model

--- hms-backend/api/models/User.js
+++ hms-backend/api/models/User.js
@@ -2,9 +2,11 @@ import mongoose from 'mongoose';
 
 const userSchema = ({
     _id: mongoose.Schema.Types.ObjectId,
-    username: {type: String, required: true},
+    first_name: {type: String, required: true},
+    last_name: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    role: {type: String, enum: ['Admin', 'Lecturer', 'Student']}
 });
 
 export default mongoose.model('User', userSchema);
