5cf4f20537e3fbb7b168b36a5a1242473cb0818d
mohhaauu <mohhaauu12@gmail.com>
2024-09-04 20:36:45 +0200

    Added middleware for user auth in check-auth.js

---
+++ hms-backend/api/middleware/check-auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+        req.userData = decodedToken;
+        next();
+    }
+    catch (error) {
+        return res.status(401).json({
+            message: 'Auth failed'
+        });
+    }
+    next();
+};
