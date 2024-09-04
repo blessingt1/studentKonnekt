b03b597eb346b850fceb7abc2c792217a48e8a03
mohhaauu <mohhaauu12@gmail.com>
2024-09-04 15:04:31 +0200

    Update role on User.js

--- hms-backend/api/models/User.js
+++ hms-backend/api/models/User.js
@@ -6,7 +6,11 @@ const userSchema = ({
     last_name: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
-    role: {type: String, enum: ['Admin', 'Lecturer', 'Student']}
+    role: {type: String, enum: [0, 1, 2], validate: {
+            validator: function(x) {
+            return [0, 1, 2].includes(x)
+        }}
+    }
 });
 
 export default mongoose.model('User', userSchema);
