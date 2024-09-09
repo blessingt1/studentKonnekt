ce6d62d6e25651b43f83ce3ecab1b10c6d64f87b
mohhaauu <mohhaauu12@gmail.com>
2024-09-09 17:34:46 +0200

    Updated the User model

--- hms-backend/api/models/User.js
+++ hms-backend/api/models/User.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
-const userSchema = ({
+const userSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     first_name: {type: String, required: true},
     last_name: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    role: {type: String, enum: [0, 1, 2], validate: {
+    role: {type: Number, enum: [0, 1, 2], validate: {
             validator: function(x) {
             return [0, 1, 2].includes(x)
         }}
