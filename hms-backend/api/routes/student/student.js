46d6dea27fa190b8ecb6253b72b52b0d96b9f652
mohhaauu <mohhaauu12@gmail.com>
2024-09-13 22:39:14 +0200

    Added view assignments route in student.js file

--- hms-backend/api/routes/student/student.js
+++ hms-backend/api/routes/student/student.js
@@ -4,13 +4,42 @@ const mongoose = require('mongoose');
 const User = require('../../models/User');
 const Assignment = require('../../models/Assignment');
 
+router.get('/View%20Assignments', (req, res, next) => {
+    Assignment.find()
+    .exec()
+    .then(result => {
+        console.log(result);
+        if (result.length >= 0) {
+            res.status(200).json(result);
+        }
+        else {
+            res.status(500).json({
+                message: 'No assignment found'
+            });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+})
+
 router.get('/:assignmentId', (req, res, next) => {
     const id = req.body.assignmentId;
     Assignment.findById(id)
     .exec()
     .then(result => {
         console.log(result);
-        res.status(200).json(result);
+        if (result){
+            res.status(200).json(result);
+        }
+        else {
+            res.status(404).json({
+                message: 'No valid entry found for the provided ID'
+            });
+        }
     })
     .catch(err => {
         console.log(err);
