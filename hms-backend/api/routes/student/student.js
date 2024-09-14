e21db22b67dcdeeb7e00494f4c4195d9113afc4d
mohhaauu <mohhaauu12@gmail.com>
2024-09-14 23:29:52 +0200

    Storage configuration for video submission

--- hms-backend/api/routes/student/student.js
+++ hms-backend/api/routes/student/student.js
@@ -3,6 +3,36 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../../models/User');
 const Assignment = require('../../models/Assignment');
+const Video = require('../../models/Video');
+const path = require('path');
+const multer = require('multer');
+
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/ ')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
+
+const file_filter = (req, file, cb) => {
+    if(file.mimetype.startsWith('video/')) {
+        cb(null, true);
+    }
+    else {
+        cb(new Error('Not a video file.'), false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: file_filter,
+    limits: {
+        fileSize: 1024 * 1024 * 100
+    }
+});
 
 router.get('/View%20Assignments', (req, res, next) => {
     Assignment.find()
@@ -49,14 +79,28 @@ router.get('/:assignmentId', (req, res, next) => {
     });
 });
 
-router.post('/Upload%20Video', (req, res, next) => {
-    const student = new User({
-        first_name,
-        last_name,
-        email,
-        password,
-        role
-    });
-    
+router.post('/Upload%20Video', upload.single('video'), (req, res) => {
+    if(!req.file){
+        res.status(400).json({
+            message: 'No video file uploaded'
+        });
+    }
+
+    try {
+        const video = new Video({
+            fileName: req.body.filename,
+            filePath: req.body.path,
+            fileSize: req.body.size,
+            fileType: req.body.mimetype,
+            uploadedBy: req.params.userId
+        });
+        video.save()
+        res.status(201).json({
+            message: 'Video uploaded successfully'
+        });
+    }
+    catch(error) {
+        res.status(400).json(error.message);
+    }
 });
 module.exports = router;
