b63ee3c493b6088252a768b09c6a10cb67f3a78d
mohhaauu <mohhaauu12@gmail.com>
2024-09-14 22:49:49 +0200

    Updated the Video model

--- hms-backend/api/models/Video.js
+++ hms-backend/api/models/Video.js
@@ -10,7 +10,28 @@ const videoSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
-    videoURL: {type: String, required: true}
+    fileName: {
+        type: String,
+        required: true
+    },
+      filePath: {
+        type: String,
+        required: true
+    },
+      fileSize: {
+        type: Number,
+        required: true
+    },
+      fileType: {
+        type: String,
+        required: true
+    },
+    uploadedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    timestamps: true
 });
 
 export default mongoose.model('Video', videoSchema);
