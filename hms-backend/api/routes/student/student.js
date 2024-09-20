5719bc200afa56fcc4b121db9bcc5946df048ab7
mohhaauu <mohhaauu12@gmail.com>
2024-09-20 15:47:22 +0200

    Moved the controller code from routes to the controller file

--- hms-backend/api/routes/student/student.js
+++ hms-backend/api/routes/student/student.js
@@ -2,11 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import path from 'path';
 import multer from 'multer';
-
 import User from '../../models/User.js';
 import Assignment from '../../models/Assignment.js';
 import Video from '../../models/Video.js';
 import Feedback from '../../models/Feedback.js';
+import checkAuth from '../../middleware/check-auth.js';
+import assignmentController from '../../controllers/assignment.js';
 
 const router = express.Router();
 
@@ -35,48 +36,10 @@ const upload = multer({
     }
 });
 
-router.get('/View%20Assignments', (req, res, next) => {
-    Assignment.find()
-    .exec()
-    .then(result => {
-        console.log(result);
-        if (result.length >= 0) {
-            res.status(200).json(result);
-        } else {
-            res.status(500).json({
-                message: 'No assignment found'
-            });
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
-
-router.get('/:assignmentId', (req, res, next) => {
-    const id = req.params.assignmentId;
-    Assignment.findById(id)
-    .exec()
-    .then(result => {
-        console.log(result);
-        if (result) {
-            res.status(200).json(result);
-        } else {
-            res.status(404).json({
-                message: 'No valid entry found for the provided ID'
-            });
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+// View Assignment
+router.get('/', checkAuth, assignmentController.getAllAssignments);
+// View Assignment by id
+router.get('/:assignmentId', checkAuth, assignmentController.getAssignmentById);
 
 router.post('/', upload.single('videoUrl'), (req, res) => {
     if (!req.file) {
@@ -87,7 +50,7 @@ router.post('/', upload.single('videoUrl'), (req, res) => {
 
     try {
         const video = new Video({
-            _id: new mongoose.Types.ObjectId,
+            _id: new mongoose.Types.ObjectId(),
             assignmentId : req.body.assignmentId,
             userId: req.body.userId,
             videoUrl: req.file.path
