efd130927ad91d3590fef3c3da844296bfd25683
mohhaauu <mohhaauu12@gmail.com>
2024-09-22 16:59:29 +0200

    Updated the user model for better clarity of roles

--- hms-backend/api/models/User.js
+++ hms-backend/api/models/User.js
@@ -1,15 +1,27 @@
 import mongoose from 'mongoose';
 
+export const USER_ROLES = {
+    ADMIN: 0,
+    LECTURER: 1,
+    STUDENT: 2
+};
+
 const userSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     first_name: {type: String, required: true},
     last_name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    role: {type: Number, enum: [0, 1, 2], validate: {
+    role: {
+        type: Number, 
+        enum: Object.values(USER_ROLES),
+        required: true,
+        validate: {
             validator: function(x) {
-            return [0, 1, 2].includes(x)
-        }}
+                return Object.values(USER_ROLES).includes(x);
+            },
+            message: props => `${props.value} is not a valid role`
+        }
     }
 });
 
