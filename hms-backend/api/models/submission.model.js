0315ae6ff7c041e4974f22e5345d7ba772aa6e9b
Blessing <162319665+blessingt1@users.noreply.github.com>
2024-09-25 19:30:35 +0200

    Update submission.model.js

--- hms-backend/api/models/submission.model.js
+++ hms-backend/api/models/submission.model.js
@@ -4,8 +4,6 @@ const submissionSchema = new mongoose.Schema({
     assignment: { type: mongoose.Schema.Types.ObjectId, ref: 'Assignment', required: true }, // Refers to the assignment
     student: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Refers to the student who submitted the video
     videoPath: { type: String, required: true }, // Path to the video file
-    feedback: { type: String },
-    marks: { type: Number },
     feedbackBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Refers to the lecturer giving feedback
     submittedAt: { type: Date, default: Date.now } // Automatically sets submission time
 }, {
