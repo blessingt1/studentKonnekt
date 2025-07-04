0911699b091d8a053e61f451e3ba9100528c5487
blessingtb3 <blessingtb3@gmail.com>
2025-07-04 23:25:07 +0200

    (client-home): fix: styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -98,16 +98,16 @@ const Home = () => {
           <h1 className="text-white mt-4 mb-4" style={{ fontSize: '2rem' }}>HMS</h1>
           <h1 className="text-white display-1 mb-5" style={{ fontSize: '2.5rem' }}>Management Platform</h1> 
           <div className="mx-auto mb-5" style={{ width: '100%', maxWidth: '600px' }}>
-            <div className="input-group">
+            <div className="input-group" >
               <div className="input-group-prepend">
-                <button className="btn btn-outline-light bg-white text-body px-4 dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Courses</button>
+                <button className="btn btn-outline-light bg-white text-body py-0 dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >Courses</button>
                 <div className="dropdown-menu">
                   <a className="dropdown-item" href="#">CMPG321</a>
                   <a className="dropdown-item" href="#">CMPG323</a>
                   <a className="dropdown-item" href="#">CMPG324</a>
                 </div>
               </div>
-              <input type="text" className="form-control border-light" style={{ padding: '21.6px 25px' }} placeholder="Keyword" />
+              <input type="text" className="form-control border-light"  placeholder="Keyword" />
               <div className="input-group-append">
                 <button className="btn btn-secondary px-4 px-lg-5">Search</button>
               </div>
